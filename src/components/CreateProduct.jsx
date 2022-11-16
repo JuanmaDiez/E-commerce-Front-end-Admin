4b98c87fe7aa2b04847d3b32c1f027104dec7fe0
Juan Manuel Diez <jm10yap@gmail.com>
2022-11-16 14:29:09 -0300

    adding category to create product

--- src/components/CreateProduct.jsx
+++ src/components/CreateProduct.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import styles from "../modules/CreateProduct.module.css";
 import { add_product } from "../redux/productsSlice";
@@ -7,10 +7,25 @@ import { add_product } from "../redux/productsSlice";
 function CreateProduct({ display, setDisplay, setBlur }) {
   const dispatch = useDispatch();
   const [featured, setFeatured] = useState(false);
+  const [categories, setCategories] = useState(null);
+  const [category, setCategory] = useState("");
+
+  useEffect(() => {
+    const getCategories = async () => {
+      const response = await axios({
+        url: `${process.env.REACT_APP_API_URL}/categories`,
+        method: "GET",
+      });
+      setCategories(response.data);
+    };
+    getCategories();
+  }, []);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
+    setBlur("blur(0px)");
+    setDisplay("d-none");
     const response = await axios({
       url: `${process.env.REACT_APP_API_URL}/products`,
       method: "POST",
@@ -18,57 +33,71 @@ function CreateProduct({ display, setDisplay, setBlur }) {
       headers: { "Content-Type": "multipart/form-data" },
     });
     dispatch(add_product(response.data));
-    setBlur("blur(0px)");
-    setDisplay("d-none");
   };
 
   return (
-    <div className={`${display} flex-column ${styles.createContainer} p-4`}>
-      <h5>Create product</h5>
-      <form
-        action=""
-        onSubmit={(event) => handleSubmit(event)}
-        className="container"
-      >
-        <div className={`form-group mt-1`}>
-          <label htmlFor="">Name</label>
-          <input type="text" className={`form-control`} name="name" />
-        </div>
-        <div className={`form-group mt-1`}>
-          <label htmlFor="">Description</label>
-          <textarea className={`form-control`} name="description"></textarea>
-        </div>
-        <div className={`form-group mt-1`}>
-          <label htmlFor="">Category</label>
-          <input type="text" className={`form-control`} name="category" />
-        </div>
-        <div className={`form-group mt-1`}>
-          <label htmlFor="">Image</label>
-          <input type="file" className={`form-control`} name="image" />
+    categories && (
+      <div className={`${display} flex-column ${styles.createContainer} p-4`}>
+        <h5>Create product</h5>
+        <form
+          action=""
+          onSubmit={(event) => handleSubmit(event)}
+          className="container"
+        >
           <div className={`form-group mt-1`}>
-            <label htmlFor="">Price</label>
-            <input type="number" className={`form-control`} name="price" />
+            <label htmlFor="">Name</label>
+            <input type="text" className={`form-control`} name="name" />
           </div>
-        </div>
-        <div className={`form-group mt-1`}>
-          <label htmlFor="">Stock</label>
-          <input type="number" className={`form-control`} name="stock" />
-        </div>
-        <div className={`form-group mt-1`}>
-          <label htmlFor="">Featured</label>
-          <input
-            type="checkbox"
-            name="featuredProduct"
-            checked={featured}
-            value={featured}
-            onChange={() => setFeatured(!featured)}
-          />
-        </div>
-        <button type="submit" className="btn btn-success mt-1">
-          Create
-        </button>
-      </form>
-    </div>
+          <div className={`form-group mt-1`}>
+            <label htmlFor="">Description</label>
+            <textarea className={`form-control`} name="description"></textarea>
+          </div>
+          <div className={`form-group mt-1`}>
+            <label htmlFor="">Category</label>
+            <input type="hidden" name="category" value={category} />
+            <div className="d-flex justify-content-around" >
+              {categories.map((category) => {
+                return (
+                  <div key={category._id}>
+                    <label htmlFor="">{category.name}</label>
+                    <input
+                      type="radio"
+                      className="ms-2"
+                      onClick={() => setCategory(category._id)}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className={`form-group mt-1`}>
+            <label htmlFor="">Image</label>
+            <input type="file" className={`form-control`} name="image" />
+            <div className={`form-group mt-1`}>
+              <label htmlFor="">Price</label>
+              <input type="number" className={`form-control`} name="price" />
+            </div>
+          </div>
+          <div className={`form-group mt-1`}>
+            <label htmlFor="">Stock</label>
+            <input type="number" className={`form-control`} name="stock" />
+          </div>
+          <div className={`form-group mt-1`}>
+            <label htmlFor="">Featured</label>
+            <input
+              type="checkbox"
+              name="featuredProduct"
+              checked={featured}
+              value={featured}
+              onChange={() => setFeatured(!featured)}
+            />
+          </div>
+          <button type="submit" className="btn btn-success mt-1">
+            Create
+          </button>
+        </form>
+      </div>
+    )
   );
 }
 
